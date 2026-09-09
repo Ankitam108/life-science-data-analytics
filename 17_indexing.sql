92e8d177a19961735f348ab2ad7048ff2e22a769
Ankitam108 <mahajanankita074@gmail.com>
2026-09-09 15:51:36 +0530

    Added indexing and search optimization queries
    Added search optimization for 'name' and 'city' columns in the patients table. Included performance analysis using EXPLAIN for city-based query.

---
+++ 17_indexing.sql
@@ -0,0 +1,37 @@
+- Q1. SEARCH OPTIMIZATION: Add search optimization to
+--     the patients table to improve searches on the
+--     patient name column.
+SELECT * FROM patients;
+
+ALTER TABLE patients
+ADD SEARCH OPTIMIZATION
+ON EQUALITY (name);
+
+SHOW TABLES LIKE 'patients';
+
+SELECT name
+FROM patients
+WHERE name = 'vikas kumar';
+
+-- Q2. SEARCH OPTIMIZATION: Add search optimization to
+--     the patients table to improve equality searches
+--     on the city column.
+SELECT * FROM patients;
+
+ALTER TABLE patients
+ADD SEARCH OPTIMIZATION
+ON EQUALITY (city);
+
+SHOW TABLES LIKE 'patients';
+
+SELECT name, city
+FROM patients
+WHERE city = 'gurugram';
+
+-- Q5. PERFORMANCE ANALYSIS: Use EXPLAIN to examine the
+--     query execution plan for a query that filters
+--     patients by city.
+EXPLAIN
+SELECT name, city
+FROM patients
+WHERE city = 'gurugram';
