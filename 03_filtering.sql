759bb8a4ccb208282823ea0ce0ab591d2135d039
Ankitam108 <mahajanankita074@gmail.com>
2026-09-21 16:20:57 +0530

    Add SQL queries for patient filtering examples

--- 03_filtering.sql
+++ 03_filtering.sql
@@ -1,27 +1,59 @@
 
--- Q6. WHERE + AND: Find patients who are older than 35 and have diabetes
-SELECT *
+-- Q1. WHERE + AND:
+-- Find patients who are older than 35 and have diabetes
+SELECT 
+ name,
+ age,
+ gender,
+ disease
 FROM patients
-WHERE  age > 35
-AND disease ='diabetes';
+WHERE  
+ age > 35
+AND
+ disease ='diabetes';
 
--- Q7. WHERE + OR: Find patients who are from Chandigarh or Amritsar
-SELECT *
+-- Q2. WHERE + OR: 
+-- Find patients who are from Chandigarh or Amritsar
+SELECT 
+ name,
+ age,
+ gender,
+ city,
+ disease
 FROM patients 
-WHERE city = 'chandigarh' 
- OR   city = 'amritsar';
+WHERE 
+ city = 'chandigarh' 
+ OR
+ city = 'amritsar';
 
--- Q8. IN: Find patients who are from Chandigarh, Amritsar, or Ludhiana
-SELECT *
+-- Q3. IN: 
+-- Find patients who are from Chandigarh, Amritsar, or Ludhiana
+SELECT 
+ name,
+ gender,
+ disease,
+ city
 FROM patients
-WHERE city IN('chandigarh', 'amritsar', 'ludhiana');
+WHERE 
+ city IN
+ ('chandigarh', 'amritsar', 'ludhiana');
 
--- Q9. BETWEEN: Find patients whose age is between 30 and 50
-SELECT *
+-- Q4. BETWEEN: 
+-- Find patients whose age is between 30 and 50
+SELECT 
+ name,
+ age,
+ disease
 FROM patients
-WHERE age BETWEEN 30 AND 50;
+WHERE 
+ age BETWEEN 30 AND 50;
 
--- Q10. LIKE: Find patients whose name starts with the letter A
-SELECT *
+-- Q5. LIKE: Find patients whose name starts with the letter A
+SELECT 
+ name,
+ age,
+ gender,
+ city
 FROM patients
-WHERE name LIKE 'a%';
+WHERE 
+ name LIKE 'a%';
